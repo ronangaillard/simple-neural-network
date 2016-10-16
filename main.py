e8243595ac5c2ba2d6d1bc65cc9823ad96325be6
Ronan Gaillard <ronan.gaillard@live.fr>
2016-10-16 13:55:20 +0200

    Update testing script

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import numpy as np
 from neuralnetwork import NeuralNetwork
+import matplotlib.pyplot as plt
 
-scalar = 3
+scalar = 1
 
 nn = NeuralNetwork(2, 1, 3)
 
-custom_input = np.matrix([ [3, 10], [2, 3], [7, 8] ])
-expected_output = np.matrix( [ [4], [1], [2]])
+custom_input = np.matrix([ [3, 5], [5, 1], [10, 2] ] , dtype=float)
+expected_output = np.matrix( [ [75], [82], [93]], dtype=float)
 
-for i in range(0,40):
+# Normalise data
+custom_input = custom_input / np.amax(custom_input, axis=0)
+expected_output = expected_output / 100
+
+cost_array = []
+
+for i in range(0,1000):
 
     cost = nn.cost(custom_input, expected_output)
     gradientW1, gradientW2 = nn.compute_gradient(custom_input, expected_output)
 
-    print "Cost : ", cost
+    #print "Cost : ", cost
+    cost_array.append(cost)
 
     nn.W1 += gradientW1 * scalar
     nn.W2 += gradientW2 * scalar
 
 output = nn.process(custom_input)
-print "Output :", output
+print "Output :", output*100
+
+plt.plot(cost_array)
+plt.show()
